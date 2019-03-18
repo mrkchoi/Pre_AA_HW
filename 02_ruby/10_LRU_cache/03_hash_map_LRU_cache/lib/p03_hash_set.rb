3c53ffff0152193595d449eea22739f4d891040f
Kenneth Choi <kennethichoi@gmail.com>
2019-03-18 15:27:18 -0700

    Implement hashset with dynamic resizing

--- 02_ruby/10_LRU_cache/03_hash_map_LRU_cache/lib/p03_hash_set.rb
+++ 02_ruby/10_LRU_cache/03_hash_map_LRU_cache/lib/p03_hash_set.rb
@@ -7,18 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    self[key.hash] << key
+    @count += 1
+    
+    resize! if @count == num_buckets
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,16 @@ class HashSet
   end
 
   def resize!
+    # double num_buckets
+    new_store = Array.new(num_buckets * 2) {Array.new}
+    @count = 0
+    # reinsert all elements back into new store
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_store.insert(el)
+      end
+    end
+    # set old store to new store
+    @store = new_store
   end
 end
