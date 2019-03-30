2ae4ea753ee61c15b392f1f96a17c33b40b1411d
Kenneth Choi <kennethichoi@gmail.com>
2019-03-30 12:06:50 -0700

    Init user session project

---
+++ 04_rails/03_rails_auth/01_user_sessions/user_sessions/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :username, :password_digest, presence: true
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+end
