cb26337c7dc391d8d6e3412f5aac365b025acc69
Kenneth Choi <kennethichoi@gmail.com>
2019-03-30 16:03:03 -0700

    Implement user model | constraints, validations & user model auth methods

---
+++ 04_rails/03_rails_auth/02_rails_auth/99_cats_II/app/models/user.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#
+
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :user_name, :password_digest, :session_token, presence: true
+  after_initialize :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+end
---
+++ 04_rails/03_rails_auth/02_rails_auth/99_cats_II/db/migrate/20190330225011_create_user_table.rb
@@ -0,0 +1,12 @@
+class CreateUserTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :user_name, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+    end
+
+    add_index :users, :user_name
+    add_index :users, :session_token, unique: true
+  end
+end
