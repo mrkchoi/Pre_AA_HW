96ac7e53fd71660490c3d7b0ec5c9a9aef511ded
Kenneth Choi <kennethichoi@gmail.com>
2019-03-31 17:49:52 -0700

    Password digest validation for user + password setter method

--- 04_rails/04_rails_auth_II/01_music_app/MusicApp/app/models/user.rb
+++ 04_rails/04_rails_auth_II/01_music_app/MusicApp/app/models/user.rb
@@ -17,6 +17,23 @@ class User < ApplicationRecord
     SecureRandom::urlsafe_base64(16)
   end
 
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
   
   private
