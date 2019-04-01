dbd6fea4b4cd3624d03aad3c9101b98fa99ba664
Kenneth Choi <kennethichoi@gmail.com>
2019-03-31 20:56:19 -0700

    Add bands table, model, routes, controller

---
+++ 04_rails/04_rails_auth_II/01_music_app/MusicApp/app/controllers/bands_controller.rb
@@ -0,0 +1,48 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all.order('bands.created_at ASC')
+    render :index
+  end
+  
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def create
+    band = Band.new(band_params)
+
+    if band.save
+      redirect_to bands_url
+    else
+      render json: 'Band name invalid!'
+    end
+
+  end
+
+  def edit
+    @band = Band.find_by(id: params[:id])
+
+    render :edit
+  end
+
+  def update
+    band = Band.find_by(id: params[:id])
+    band.update_attributes(band_params)
+    redirect_to bands_url
+  end
+
+  def show
+  end
+
+
+
+  def destroy
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end
+end
--- 04_rails/04_rails_auth_II/01_music_app/MusicApp/app/helpers/application_helper.rb
+++ 04_rails/04_rails_auth_II/01_music_app/MusicApp/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def auth_token
+    "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe
+  end
+
+  def patch_input
+    "<input type=\"hidden\" name=\"_method\" value=\"PATCH\">".html_safe
+  end
 end
