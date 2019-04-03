7215ed5ab16fda95fea985e8a336a3bd745b1ec1
Kenneth Choi <kennethichoi@gmail.com>
2019-04-03 10:16:42 -0700

    Add goal commenting feature

--- 04_rails/05_testing_with_rails/03_goal_app/GoalApp/config/routes.rb
+++ 04_rails/05_testing_with_rails/03_goal_app/GoalApp/config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users do
-    resources :goals
+    resources :goals do 
+      resources :goal_comments
+    end
     resources :user_comments
   end
   resources :sessions
   resources :user_comments
+  resource :goal_comments
 end
