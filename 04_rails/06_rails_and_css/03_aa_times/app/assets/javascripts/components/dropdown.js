cb679c672688747c49ac7c4bb44ad9cc8047f047
Kenneth Choi <kennethichoi@gmail.com>
2019-04-03 21:40:34 -0700

    Complete aa times layout + styling

--- 04_rails/06_rails_and_css/03_aa_times/app/assets/javascripts/components/dropdown.js
+++ 04_rails/06_rails_and_css/03_aa_times/app/assets/javascripts/components/dropdown.js
@@ -4,7 +4,8 @@
 // adding hideDropdown listener to document and cleaning up out-of-date listener
 const revealDropdown = (event) => {
   event.stopPropagation(); // prevent event from being picked up by body
-	$('#gear-dropdown').removeClass('hidden');
+  $('#gear-dropdown').removeClass('hidden');
+  console.log('clicked on gear icon!');
   $('#gear-dropdown-btn').off('click', revealDropdown);
   $(document).on('click', hideDropdown);
 };
@@ -14,6 +15,7 @@ const hideDropdown = () => {
 	$('#gear-dropdown').addClass('hidden');
   $('#gear-dropdown-btn').on('click', revealDropdown);
   $(document).off('click', hideDropdown);
+  console.log("clicked on gear icon!");
 };
 
 // Add click listener to gear icon which invokes reveal function
