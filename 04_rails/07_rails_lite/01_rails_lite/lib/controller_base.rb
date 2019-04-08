50bd418d062d60dc86b90a649265a0115ee2a79b
Kenneth Choi <kennethichoi@gmail.com>
2019-04-07 22:22:21 -0700

    Implement PI - PIV RailsLite

--- 04_rails/07_rails_lite/01_rails_lite/lib/controller_base.rb
+++ 04_rails/07_rails_lite/01_rails_lite/lib/controller_base.rb
@@ -2,35 +2,53 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'active_support/inflector'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'Double render error!' if already_built_response?
+    @res.status = 302
+    @res.location = url
+    @already_built_response = true
+    @session.store_session(@res)
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res['Content-Type'] = content_type
+    raise 'Render cannot be called twice' if already_built_response?
+    @res.write(content)
+    @already_built_response = true
+    @session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+    erb_template = ERB.new(File.read(path)).result(binding)
+    render_content(erb_template, 'text/html')
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
