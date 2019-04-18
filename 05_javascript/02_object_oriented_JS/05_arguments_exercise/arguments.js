521b616e31a1d76721d39370c642681bc4c8d95e
Kenneth Choi <kennethichoi@gmail.com>
2019-04-18 15:32:01 -0700

    Implement #sum via arguments & rest operator

---
+++ 05_javascript/02_object_oriented_JS/05_arguments_exercise/arguments.js
@@ -0,0 +1,28 @@
+
+
+// function sum() {
+//   let sum = 0;
+  
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// }
+
+// console.log(sum(1, 2, 3, 4));
+
+
+// function sum() {
+//   let sum = 0;
+//   let args = [...arguments];
+
+//   args.forEach(el => sum += el);
+//   return sum;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+
+
+
+
+
