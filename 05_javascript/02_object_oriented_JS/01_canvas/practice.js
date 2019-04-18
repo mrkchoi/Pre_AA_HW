94d2fb01c422f7d1b572933e25a69fc867cf4424
Kenneth Choi <kennethichoi@gmail.com>
2019-04-18 14:30:31 -0700

    ComplComplete CSSFreiends Phase 7

--- 05_javascript/02_object_oriented_JS/01_canvas/practice.js
+++ 05_javascript/02_object_oriented_JS/01_canvas/practice.js
@@ -1,6 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
   const canvas =  document.getElementById('mycanvas');
   const ctx = canvas.getContext('2d');
-  ctx.fillStyle = ('red');
-  ctx.fillRect(50,50,100,100);
+  // ctx.fillStyle = ('red');
+  // // ctx.fillRect(50,50,100,100);
+  // // ctx.beginPath();
+  // // ctx.arc(50, 50, 40, 20, 30);
+  // // ctx.strokeStyle = 'orangered';
+  // // ctx.lineWidth = 3;
+  // // ctx.stroke();
+  // // ctx.fillStyle = 'pink';
+  // // ctx.fill();
+  // // ctx.closePath();
+
+  // ctx.beginPath();
+  // ctx.moveTo(75, 50);
+  // ctx.lineTo(100, 75);
+  // ctx.lineTo(100, 25);
+  // ctx.lineTo(100, 100);
+  // ctx.lineTo(100, 20);
+  // ctx.fill();
+
+
+  // ctx.beginPath();
+  // ctx.arc(75,75,50,0, Math.PI * 2, true);
+  // ctx.moveTo(110, 75);
+  // ctx.arc(75,75,35,0,Math.PI, false);
+  // ctx.moveTo(65, 65);
+  // ctx.arc(60,65,5,0, Math.PI * 2, true);
+  // ctx.moveTo(95,65);
+  // ctx.arc(90,65,5,0, Math.PI * 2, true);
+  // ctx.lineWidth = 2;
+  // ctx.stroke();
+  
+  ctx.beginPath();
+  ctx.moveTo(25,25);
+  ctx.lineTo(75,75);
+  ctx.lineTo(25,75);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fill();
 });
