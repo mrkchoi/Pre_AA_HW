b2f26d9ed97251f60684a92dcaf88e0778c704ac
Kenneth Choi <kennethichoi@gmail.com>
2019-04-18 11:38:23 -0700

    Init canvas demo

---
+++ 05_javascript/02_object_oriented_JS/01_canvas/practice.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas =  document.getElementById('mycanvas');
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = ('red');
+  ctx.fillRect(50,50,100,100);
+});
