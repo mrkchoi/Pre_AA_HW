4330a003d7a2c6b39c996029d24bd6bd17311828
Kenneth Choi <kennethichoi@gmail.com>
2019-04-18 22:02:50 -0700

    Complete #sortedInsert (linkedlist) HR

--- 05_javascript/02_object_oriented_JS/_00/algo.rb
+++ 05_javascript/02_object_oriented_JS/_00/algo.rb
@@ -8,24 +8,80 @@
 #     SinglyLinkedListNode next
 #
 #
-def insertNodeAtPosition(llist, data, position)
-  new_node = SinglyLinkedListNode.new(data)
 
-  if position == 0
-    new_node.next = llist
+# def insertNodeAtPosition(llist, data, position)
+#   new_node = SinglyLinkedListNode.new(data)
+  
+#   if position == 0
+#     new_node.next = llist
+#     return new_node
+#   end
+
+#   count = 0
+#   current_node = llist
+
+#   while count < position - 1
+#     current_node = current_node.next
+#     count += 1
+#   end
+
+#   new_node.next = current_node.next
+#   current_node.next = new_node
+
+#   llist
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def sortedInsert(head, data)
+  new_node = DoublyLinkedListNode.new(data)
+  if head.nil?
+    return new_node 
+  end
+
+
+
+  current_node = head
+  
+  if current_node.data > new_node.data
+    new_node.next = current_node
+    current_node.prev = new_node
     return new_node
   end
 
-  count = 0
-  current_node = llist
+  while true
+    if current_node.next.nil?
+      new_node.next = current_node.next
+      new_node.prev = current_node
+      current_node.next = new_node
+      return head
+    end
 
-  while count < position - 1
-    current_node = current_node.next
-    count += 1
+    if current_node.next.data > new_node.data
+      new_node.next = current_node.next
+      new_node.prev = current_node
+      current_node.next = new_node
+      if !new_node.next.nil?
+        new_node.next.prev = new_node
+      end
+      
+      return head
+    else
+      current_node = current_node.next
+    end
   end
-
-  new_node.next = current_node.next
-  current_node.next = new_node
-
-  llist
 end
