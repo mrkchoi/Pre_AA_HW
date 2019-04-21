af35abf1dd6ca21c4940ef2d0a3614a383d96638
Kenneth Choi <kennethichoi@gmail.com>
2019-04-21 14:14:16 -0700

    Init wackyjQ

--- 05_javascript/03_jQuery/_00/algos.rb
+++ 05_javascript/03_jQuery/_00/algos.rb
@@ -32,3 +32,104 @@ def reverse(head, prev_node = nil)
   head.next = prev_node
   return reverse(new_head, head)
 end
+
+
+
+
+
+def generate(num_rows)
+    return nil if num_rows == 0
+    return [[1]] if num_rows == 1
+    return [[1, 1]] if num_rows == 1
+    output = [[1], [1,1]]
+
+    until output.length == num_rows
+      last_row = output[-1]
+      cur_row = [1]
+      
+      i = 0
+      while i < last_row.length - 1
+        pair_sum = last_row[i] + last_row[i + 1]
+        cur_row << pair_sum        
+        i += 1
+      end
+      cur_row << 1
+      output << cur_row
+    end
+    
+    output
+end
+
+
+
+
+
+
+
+
+
+
+def get_row(row_index)
+    return [1] if row_index == 0
+    return [1,1] if row_index == 1
+
+    count = 1
+    prev_row = [1,1]
+    
+    (row_index - 1).times do
+      count += 1
+      cur_row = [1]
+
+      i = 0
+      while i < prev_row.length - 1
+        pair_sum = prev_row[i] + prev_row[i + 1]
+        cur_row << pair_sum
+        i += 1
+      end
+
+      cur_row << 1
+      prev_row = cur_row
+    end
+
+    prev_row
+end
+
+
+
+
+
+
+
+def sorted_array_to_bst(nums)
+  return nil if nums.nil? || nums.empty?
+
+  mid = nums.length / 2
+  n = TreeNode.new(nums[mid])
+  
+  n.left = sorted_array_to_bst(nums[0...mid])
+  n.right = sorted_array_to_bst(nums[mid+1..-1])
+
+  n
+end
+
+
+
+
+
+
+
+
+
+
+
+def sorted_array_to_bst(nums)
+  return nil if nums.nil? || nums.empty?
+
+  mid = nums / 2
+  node = TreeNode.new(nums[mid])
+
+  node.left = sorted_array_to_bst(arr[0...mid])
+  node.right = sorted_array_to_bst(arr[mid+1..-1])
+  
+  node
+end
