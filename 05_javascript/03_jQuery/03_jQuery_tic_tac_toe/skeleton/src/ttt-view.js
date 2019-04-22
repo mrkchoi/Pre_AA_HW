5646f183a85b3ab37beb2d4471692f01c3714afd
Kenneth Choi <kennethichoi@gmail.com>
2019-04-22 11:40:37 -0700

    Complete jQuery ttt

--- 05_javascript/03_jQuery/03_jQuery_tic_tac_toe/skeleton/src/ttt-view.js
+++ 05_javascript/03_jQuery/03_jQuery_tic_tac_toe/skeleton/src/ttt-view.js
@@ -20,6 +20,29 @@ class View {
     console.log(pos);
     this.game.playMove(pos);
     this.setupBoard();
+    $(window).on('error', function(e) {
+      alert("Invalid move!");
+    });
+    this.winner();
+  }
+
+  winner() {
+    let container = $('.ttt');
+    let winner = this.game.board.winner();
+    if (winner) {
+      let winMsg = $(`<p class="win_msg">You win, ${winner}!</p>`);
+      if (winner === 'x') {
+        container.addClass('game_winner_X');
+      } else {
+        container.addClass('game_winner_O');
+      }
+      container.append(winMsg);
+      container.addClass('game_won');
+    } else if (this.game.board.isOver() && !winner) {
+      let drawMsg = $(`<p class="win_msg">Draw game!</p>`);
+      container.addClass('draw_game');
+      container.append(drawMsg);
+    }
   }
 
   setupBoard() {
@@ -37,8 +60,10 @@ class View {
           $(square).text(' ');
         } else if (grid[i][j] === 'x') {
           $(square).text('x');
+          $(square).addClass('x_mark');
         } else if (grid[i][j] === 'o') {
           $(square).text('o');
+          $(square).addClass('o_mark');
         }
 
         board.append(square);
