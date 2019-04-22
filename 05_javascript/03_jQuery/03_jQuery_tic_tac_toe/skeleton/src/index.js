6c1f698e62873c9ed7a6df14584d81ae1125ebee
Kenneth Choi <kennethichoi@gmail.com>
2019-04-22 09:32:01 -0700

    Init jQuery ttt project

---
+++ 05_javascript/03_jQuery/03_jQuery_tic_tac_toe/skeleton/src/index.js
@@ -0,0 +1,6 @@
+const View = require('./ttt-view');// require appropriate file
+const Game = require('../../node_solution/game'); // require appropriate file
+
+  $(() => {
+    // Your code here
+  });
