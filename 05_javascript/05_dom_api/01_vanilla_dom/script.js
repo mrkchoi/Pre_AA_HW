a8e774c8d01e5ce582d468d3e77c68a56c7e0f3a
Kenneth Choi <kennethichoi@gmail.com>
2019-04-24 19:26:31 -0700

    Complete vanillaJSDOM v1

--- 05_javascript/05_dom_api/01_vanilla_dom/script.js
+++ 05_javascript/05_dom_api/01_vanilla_dom/script.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const sfPlaces = document.querySelector('#sf-places');
     let formValue = formInput.value;
     let newPlace = document.createElement('li');
-    
+
     newPlace.innerHTML = formValue;
     sfPlaces.appendChild(newPlace);
     formInput.value = '';
@@ -38,6 +38,35 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding new photos
 
   // --- your code here!
+  const photoFormContainer = document.querySelector('.photo-form-container');
+  const photoShowButton = document.querySelector('.photo-show-button');
+
+  photoShowButton.addEventListener('click', e => {
+    // console.log('clicked on toggle!');
+    photoFormContainer.classList.toggle('hidden');
+  });
+
+  const photoForm = document.querySelector('.photo-form-container form');
+
+  photoForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let photoUrlInput = document.querySelector('.photo-url-input');
+    let newPhotoItem = document.createElement('li');
+    let newPhoto = document.createElement('img');
+    let newPhotoUrl = photoUrlInput.value;
+
+    newPhoto.setAttribute('src', newPhotoUrl);
+    newPhotoItem.appendChild(newPhoto);
+
+    let photoGallery = document.querySelector('.dog-photos');
+    photoGallery.appendChild(newPhotoItem);
+    
+    photoUrlInput.value = '';
+    
+  });
+
+
 
 
 
