4f6a4a7f6d6239b822e206ad486e30b9dc4252f2
Kenneth Choi <kennethichoi@gmail.com>
2019-04-24 19:16:23 -0700

    Complete form submission reflect in DOM

--- 05_javascript/05_dom_api/01_vanilla_dom/script.js
+++ 05_javascript/05_dom_api/01_vanilla_dom/script.js
@@ -19,6 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
+  const listForm = document.querySelector('.list-container form');
+  listForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formInput = document.querySelector('.favorite-input');
+    const sfPlaces = document.querySelector('#sf-places');
+    let formValue = formInput.value;
+    let newPlace = document.createElement('li');
+    
+    newPlace.innerHTML = formValue;
+    sfPlaces.appendChild(newPlace);
+    formInput.value = '';
+  });
 
 
 
