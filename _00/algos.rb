607637a67b2e93f710852d6826491b02d4fa32a6
Kenneth Choi <kennethichoi@gmail.com>
2019-04-24 22:04:11 -0700

    Implement CTCI 1.8 v1

--- _00/algos.rb
+++ _00/algos.rb
@@ -58,8 +58,68 @@ end
 # Space: O(n)
 
 
-p is_unique('hello') # => false
-p is_unique('abcdef') # => true
-p is_unique('aaaaaaaaaaa') # => false
-p is_unique('abcdefghijklmnopqrstuvwxyz') # => true
-p is_unique('this may be un1q') # => false
+# p is_unique('hello') # => false
+# p is_unique('abcdef') # => true
+# p is_unique('aaaaaaaaaaa') # => false
+# p is_unique('abcdefghijklmnopqrstuvwxyz') # => true
+# p is_unique('this may be un1q') # => false
+
+
+
+
+
+# CTCI 1.8
+# Zero Matrix: Write an algorithm such that if an element in an M x N matrix is 0, its entire row and column are set to 0.
+
+# Solution #1
+def zero_matrix(arr)
+  row = arr.length
+  col = arr[0].length
+  dup_arr = arr.dup
+  matched_row = []
+  matched_col = []
+
+  arr.each do |row|
+    p row
+  end
+  p '----------'
+  i = 0
+  while i < row
+    j = 0
+    while j < col
+      if arr[i][j] == 0
+        matched_row << i
+        matched_col << j
+      end      
+      j += 1
+    end
+    i += 1
+  end
+
+  i = 0
+  while i < row
+    j = 0
+    while j < col
+      if matched_row.include?(i) || matched_col.include?(j)
+        dup_arr[i][j] = 0
+      end
+      j += 1
+    end
+    i += 1
+  end
+  dup_arr.each do |row|
+    p row
+  end
+end
+
+# Time: O(n)
+# Space: O(n)
+
+
+m1 = [
+  [1,2,3,4,5],
+  [1,2,0,4,5],
+  [1,2,3,4,5],
+  [1,2,3,4,0]
+]
+zero_matrix(m1)
