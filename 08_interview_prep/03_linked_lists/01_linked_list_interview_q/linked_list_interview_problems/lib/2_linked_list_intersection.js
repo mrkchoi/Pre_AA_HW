c307b81992e9b636341bf5678e95a4e06e67ad1d
Kenneth Choi <kennethichoi@gmail.com>
2019-04-26 20:48:39 -0700

    Complete v2 #linkedListIntersection (js)

--- 08_interview_prep/03_linked_lists/01_linked_list_interview_q/linked_list_interview_problems/lib/2_linked_list_intersection.js
+++ 08_interview_prep/03_linked_lists/01_linked_list_interview_q/linked_list_interview_problems/lib/2_linked_list_intersection.js
@@ -40,7 +40,40 @@
 // Let's code!
 // -----------
 function linkedListIntersection(list1, list2) {
-  
+  if (!list1 || !list2) {
+    return null;
+  }
+
+  let list1Length = linkedListLength(list1.head);
+  let list2Length = linkedListLength(list2.head);
+  let diff = Math.abs(list2Length - list1Length);
+  let long = list1Length >= list2Length ? list1.head : list2.head;
+  let short = list1Length >= list2Length ? list2.head : list1.head;
+
+  while (long && short) {
+    if (diff > 0) {
+      long = long.next;
+      diff -= 1;
+      continue;
+    }
+
+    if (long === short) {
+      return long;
+    }
+    long = long.next;
+    short = short.next;
+  }
+  return null;
+}
+
+function linkedListLength(node) {
+  if (node === null) {
+    return null;
+  } else if (node.next === null) {
+    return 1;
+  } else {
+    return linkedListLength(node.next) + 1;
+  }
 }
 
 // ----------------------------------------
