8c50f5f819fe40f9a84ee584f03d9cdd58ab3733
Kenneth Choi <kennethichoi@gmail.com>
2019-04-27 10:49:04 -0700

    Implement LC 234: Palindrome Linked List v1

--- _00/algo.js
+++ _00/algo.js
@@ -1,3 +1,4 @@
+import { allSettled } from "q";
 
 
 // Leetcode 160: Intersection of Two Linked Lists
@@ -39,4 +40,76 @@ function getLinkedListLength(head) {
   } else {
     return getLinkedListLength(head.next) + 1;
   }
+}
+
+
+
+
+
+
+// Leetcode 141: Linked List Cycle
+var hasCycle = function (head) {
+  if (head === null || head.next === null) {
+    return false;
+  }
+
+  let walker = head;
+  let runner = head;
+
+  while (walker.next != null && runner.next != null && runner.next.next != null) {
+    walker = walker.next;
+    runner = runner.next.next;
+
+    if (walker === runner) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+
+
+
+
+
+
+// Leetcode 234: Palindrome Linked List
+var isPalindrome = function (head) {
+  // Edge case check
+  if (head === null || head.next === null) {
+    return true;
+  }
+  
+  // set walker & runner to find midpoint
+  let walker = head;
+  let runner = head;
+
+  while (runner && runner.next) {
+    runner = runner.next.next;
+    walker = walker.next;
+  }
+
+  // Reverse starting from midpoint
+  let node = null;
+  let next;
+  
+  // Iterate through first half and reversed second half
+  while (walker) {
+    next = walker.next;
+    walker.next = node;
+    node = walker;
+    walker = next;
+  }
+
+  // Check that every node is a match
+  while (node && head) {
+    if (node.val != head.val) {
+      return false;
+    }
+    node = node.next;
+    head = head.next;
+  }
+
+  return true;
 }
