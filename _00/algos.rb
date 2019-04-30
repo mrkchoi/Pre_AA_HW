c83aa389a615a8a9d3c9e3ff034fe9bded0346d3
Kenneth Choi <kennethichoi@gmail.com>
2019-04-29 21:05:57 -0700

    Implement LC 232: Implement Queue using Stacks

--- _00/algos.rb
+++ _00/algos.rb
@@ -850,3 +850,93 @@ def next_larger_nodes(head)
 
     res
 end
+
+
+
+
+
+
+
+
+# LC 232: Implement Queue using Stacks
+class MyQueue
+
+=begin
+    Initialize your data structure here.
+=end
+    def initialize()
+        @length = 0
+        @inStack = []
+        @outStack = []
+    end
+
+
+=begin
+    Push element x to the back of queue.
+    :type x: Integer
+    :rtype: Void
+=end
+    def push(x)
+        @inStack.push(x)
+    end
+
+
+=begin
+    Removes the element from in front of queue and returns that element.
+    :rtype: Integer
+=end
+    def pop()
+        return nil if @inStack.empty?
+
+        until @inStack.empty?
+          @outStack.push(@inStack.pop())
+        end
+
+        removed = @outStack.pop()
+
+        until @outStack.empty?
+          @inStack.push(@outStack.pop())
+        end
+
+        removed
+    end
+
+
+=begin
+    Get the front element.
+    :rtype: Integer
+=end
+    def peek()
+        return nil if @inStack.empty?
+
+        until @inStack.empty?
+          @outStack.push(@inStack.pop())
+        end
+        
+        peek_el = @outStack[-1]
+
+        until @outStack.empty?
+          @inStack.push(@outStack.pop())
+        end
+
+        peek_el
+    end
+
+
+=begin
+    Returns whether the queue is empty.
+    :rtype: Boolean
+=end
+    def empty()
+        @inStack.empty?
+    end
+
+
+end
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue.new()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
