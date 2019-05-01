cecd850f0c64f8d9b63af99e486c97cebc98646e
Kenneth Choi <kennethichoi@gmail.com>
2019-05-01 11:08:46 -0700

    Complete rspec a1p2

---
+++ 01b_foundations_redux/06_assessment_I/assessment_1/lib/part_2.rb
@@ -0,0 +1,17 @@
+
+def element_count(arr)
+  hash = Hash.new(0)
+  arr.each {|el| hash[el] += 1}
+  hash
+end
+
+def char_replace!(str, hash)
+  str.each_char.with_index do |char, i|
+    str[i] = hash[char] if hash[char]
+  end
+  str
+end
+
+def product_inject(arr)
+  arr.inject {|acc,el| acc * el}
+end
