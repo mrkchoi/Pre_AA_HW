0281159a0facfafd0f32b5499b85d1b4e043301c
Kenneth Choi <kennethichoi@gmail.com>
2019-04-30 17:11:32 -0700

    Init react widgets boilerplate

---
+++ 06_react/03_react_widgets/webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: './app.jsx',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  devtool: "source-map"
+};
