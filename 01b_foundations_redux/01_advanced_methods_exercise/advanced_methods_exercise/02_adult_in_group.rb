adbd99a9fefd252791113d85802118ed7f285931
Kenneth Choi <kennethichoi@gmail.com>
2019-04-30 20:41:45 -0700

    Complete #adult_in_group

--- 01b_foundations_redux/01_advanced_methods_exercise/advanced_methods_exercise/02_adult_in_group.rb
+++ 01b_foundations_redux/01_advanced_methods_exercise/advanced_methods_exercise/02_adult_in_group.rb
@@ -3,6 +3,10 @@
 # The method should return false otherwise.
 
 
+def adult_in_group?(arr)
+    arr.any? {|person| person[:age] >= 18}
+end
+
 people_1 = [
     {name: "Jack", age: 17},
     {name: "Jill", age: 21},
