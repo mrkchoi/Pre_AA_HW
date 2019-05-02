3badcdd7777513882923129aaf89b3f9de15980d
Kenneth Choi <kennethichoi@gmail.com>
2019-05-01 20:01:09 -0700

    Complete blocks p2

--- 01b_foundations_redux/09_blocks/blocks_project/lib/part_2.rb
+++ 01b_foundations_redux/09_blocks/blocks_project/lib/part_2.rb
@@ -5,3 +5,19 @@ def all_words_capitalized?(arr)
     el == el.capitalize
   end
 end
+
+def no_valid_url?(arr)
+  valid_urls = ['.com','.net','.io','.org']
+
+  arr.none? do |el|
+    tld = el.split('.')[1]
+    tld = '.' + tld
+    valid_urls.include?(tld)
+  end
+end
+
+def any_passing_students?(arr)
+  arr.any? do |student|
+    (student[:grades].sum)/(student[:grades].length).to_f >= 75
+  end
+end
