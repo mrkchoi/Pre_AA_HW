004c65b7dac579250d6fefb20520a64117011509
Kenneth Choi <kennethichoi@gmail.com>
2019-05-03 09:56:52 -0700

    Complete hangman h2p2

--- 01b_foundations_redux/17_hangman_project/hangman_project/lib/hangman.rb
+++ 01b_foundations_redux/17_hangman_project/hangman_project/lib/hangman.rb
@@ -37,4 +37,53 @@ class Hangman
       @guess_word[idx] = char
     end
   end   
+
+  def try_guess(char)
+    if already_attempted?(char)
+      p 'that has already been attempted'
+      return false
+    else
+      @attempted_chars << char
+      if get_matching_indices(char).empty?
+        @remaining_incorrect_guesses -= 1
+      else
+        matches = get_matching_indices(char)
+        fill_indices(char, matches)
+      end
+    end
+    return true
+  end
+
+  def ask_user_for_guess
+    p 'Enter a char:'
+    input = gets.chomp
+    try_guess(input)
+  end
+
+  def win?
+    if @guess_word.join('') == @secret_word
+      p 'WIN'
+      return true
+    else
+      return false
+    end
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      p 'LOSE'
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    if win? || lose?
+      p @secret_word
+      return true
+    else
+      return false
+    end
+  end
 end
