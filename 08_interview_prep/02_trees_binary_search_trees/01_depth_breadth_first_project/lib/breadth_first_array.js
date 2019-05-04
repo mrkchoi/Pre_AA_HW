8abfa58eb750254045e9775b54334b057503b4b5
Kenneth Choi <kennethichoi@gmail.com>
2019-05-03 20:45:22 -0700

    Implement #breandthFirstArray

--- 08_interview_prep/02_trees_binary_search_trees/01_depth_breadth_first_project/lib/breadth_first_array.js
+++ 08_interview_prep/02_trees_binary_search_trees/01_depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,18 @@
 function breadthFirstArray(root) {
+    if (!root) return;
 
+    let values = [];
+    let queue = [root];
+
+    while (queue.length) {
+        let node = queue.shift();
+        values.push(node.val);
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    
+    return values;
 }
 
 module.exports = {
