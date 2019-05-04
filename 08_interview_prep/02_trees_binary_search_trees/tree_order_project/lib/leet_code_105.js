084877fb08a237baa352a25b71a9cdadd3a2c7dc
Kenneth Choi <kennethichoi@gmail.com>
2019-05-03 20:00:32 -0700

    Complete #tree_order_traversals

--- 08_interview_prep/02_trees_binary_search_trees/tree_order_project/lib/leet_code_105.js
+++ 08_interview_prep/02_trees_binary_search_trees/tree_order_project/lib/leet_code_105.js
@@ -23,99 +23,176 @@
 const { TreeNode } = require('./tree_node.js');
 
 
-// function buildTree(preorder, inorder) {
-//   if (!preorder.length && !inorder.length) return null;
-
-//   let root = new TreeNode(preorder[0]);
-
-//   let midIdx = inorder.indexOf(preorder[0]);
-//   let leftInOrder = inorder.slice(0, midIdx);
-//   let rightInOrder = inorder.slice(midIdx + 1);
-
-//   let leftPreOrder = preorder.filter(val => leftInOrder.includes(val));
-//   let rightPreOrder = preorder.filter(val => rightInOrder.includes(val));
-
-//   root.left = buildTree(leftPreOrder, leftInOrder);
-//   root.right = buildTree(rightPreOrder, rightInOrder);
-
-//   return root;
-// }
-
-
-
-
 function buildTree(preorder, inorder) {
-  function helper(p0, p1, i0, i1) {
-    if (p0 > p1 || i0 > i1) return null;
-
-    let value = preorder[p0],
-      index = inorder.indexOf(value),
-      lTree = index - i0,
-      root = new TreeNode(value);
-
-    root.left = helper(p0 + 1, p1 + lTree, i0, index - 1);
-    root.right = helper(p0 + lTree + 1, p1, index + 1, i1);
-
-    return root;
+  if (!preorder.length) {
+    return null;
   }
 
-  return helper(0, preorder.length - 1, 0, inorder.length - 1);
+  let value = preorder.shift();
+  let node = new TreeNode(value);
+  let idx = inorder.indexOf(value);
+
+  node.left = buildTree(preorder, inorder.slice(0, idx));
+  node.right = buildTree(preorder, inorder.slice(idx+1));
+
+  return node;
 }
 
 
 
 
-// var buildTree = function (preorder, inorder) {
-//   if (!inorder.length) {
-//     return null;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function buildTree(preorder, inorder) {
+//   function helper(p0, p1, i0, i1) {
+//     if (p0 > p1 || i0 > i1) return null;
+
+//     let value = preorder[p0],
+//       index = inorder.indexOf(value),
+//       lTree = index - i0,
+//       root = new TreeNode(value);
+
+//     root.left = helper(p0 + 1, p1 + lTree, i0, index - 1);
+//     root.right = helper(p0 + lTree + 1, p1, index + 1, i1);
+
+//     return root;
 //   }
 
-//   var root = new TreeNode(preorder.shift());
-//   var inorderIndex = inorder.indexOf(root.val);
+//   return helper(0, preorder.length - 1, 0, inorder.length - 1);
+// }
 
-//   // divide the inorder list into left side
-//   root.left = buildTree(preorder, inorder.slice(0, inorderIndex));
-//   // divide the inorder list into right side
-//   root.right = buildTree(preorder, inorder.slice(inorderIndex + 1));
 
-//   return root;
+
+
+// // var buildTree = function (preorder, inorder) {
+// //   if (!inorder.length) {
+// //     return null;
+// //   }
+
+// //   var root = new TreeNode(preorder.shift());
+// //   var inorderIndex = inorder.indexOf(root.val);
+
+// //   // divide the inorder list into left side
+// //   root.left = buildTree(preorder, inorder.slice(0, inorderIndex));
+// //   // divide the inorder list into right side
+// //   root.right = buildTree(preorder, inorder.slice(inorderIndex + 1));
+
+// //   return root;
+// // };
+
+
+
+// var buildTree = function (preorder, inorder) {
+//   if (!inorder.length) return null;
+
+//   let newNode = new TreeNode(preorder.shift());
+//   let inorderIdx = inorder.indexOf(newNode.val);
+
+//   newNode.left = buildTree(preorder, inorder.slice(0,inorderIdx));
+//   newNode.right = buildTree(preorder, inorder.slice(inorderIdx+1));
+
+//   return newNode;
 // };
 
 
 
-var buildTree = function (preorder, inorder) {
-  if (!inorder.length) return null;
-
-  let newNode = new TreeNode(preorder.shift());
-  let inorderIdx = inorder.indexOf(newNode.val);
-
-  newNode.left = buildTree(preorder, inorder.slice(0,inorderIdx));
-  newNode.right = buildTree(preorder, inorder.slice(inorderIdx+1));
-
-  return newNode;
-};
 
 
 
 
+// var buildTree = function (preorder, inorder) {
+//   function helper(p0, p1, i0, i1) {
+//     if (p0 > p1 || i0 > i1) {
+//       return null;
+//     }
+
+//     let root = new TreeNode(preorder[p0]);
+//     let inorderIdx = inorder.indexOf(root.val);
+//     let leftNodes = inorderIdx - i0;
+
+//     root.left = helper(p0 + 1, p1 + leftNodes, i0, inorderIdx - 1);
+//     root.left = helper(p0 + leftNodes + 1, p1, inorderIdx + 1);
+
+//     return root;
+//   }
+
+//   return helper(0, preorder.length - 1, 0, inorder.length - 1);
+// };
 
 
 
-var buildTree = function (preorder, inorder) {
-  function helper(p0, p1, i0, i1) {
-    if (p0 > p1 || i0 > i1) {
-      return null;
-    }
+// return nil if preorder.none ?
 
-    let root = new TreeNode(preorder[p0]);
-    let inorderIdx = inorder.indexOf(root.val);
-    let leftNodes = inorderIdx - i0;
+//   node = TreeNode.new(preorder.first)
+//     i = inorder.index(preorder.first)
+// node.left = build_tree(preorder[1..i], inorder[0...i])
+// node.right = build_tree(preorder[i + 1..- 1], inorder[i + 1..- 1])
 
-    root.left = helper(p0 + 1, p1 + leftNodes, i0, inorderIdx - 1);
-    root.left = helper(p0 + leftNodes + 1, p1, inorderIdx + 1);
-
-    return root;
-  }
-
-  return helper(0, preorder.length - 1, 0, inorder.length - 1);
-};
+// node
