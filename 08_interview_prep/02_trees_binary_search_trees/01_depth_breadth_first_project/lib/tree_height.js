880bfa0fd111f3e7b3f0a37dc09e286853f479bf
Kenneth Choi <kennethichoi@gmail.com>
2019-05-03 20:51:56 -0700

    Implement #treeSum + #treeHeight

--- 08_interview_prep/02_trees_binary_search_trees/01_depth_breadth_first_project/lib/tree_height.js
+++ 08_interview_prep/02_trees_binary_search_trees/01_depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,24 @@
 function treeHeight(root) {
+    if (!root) {
+        return -1;
+    } else if (!root.left && !root.right) {
+        return 0;
+    }
 
+    let height = 0;
+    let queue = [root];
+
+    while (queue.length) {
+        let node = queue.shift();
+
+        if (node.left || node.right) {
+            height += 1;
+        }
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+    return height;
 }
 
 
--- 08_interview_prep/02_trees_binary_search_trees/01_depth_breadth_first_project/lib/tree_sum.js
+++ 08_interview_prep/02_trees_binary_search_trees/01_depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,19 @@
 function treeSum(root) {
+    let sum = 0;
+    if (!root) return sum;
 
+    let queue = [root];
+
+    while (queue.length) {
+        let node = queue.shift();
+
+        sum += node.val;
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return sum;    
 }
 
 
