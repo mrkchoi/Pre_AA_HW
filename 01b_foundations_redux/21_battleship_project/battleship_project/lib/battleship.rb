7e9162e6b96b7b9677e29d0c1a3873cc83d11e6c
Kenneth Choi <kennethichoi@gmail.com>
2019-05-05 10:03:32 -0700

    Complete battleship v2

--- 01b_foundations_redux/21_battleship_project/battleship_project/lib/battleship.rb
+++ 01b_foundations_redux/21_battleship_project/battleship_project/lib/battleship.rb
@@ -2,5 +2,48 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :board, :player
+  def initialize(n)
+    @player = Player.new()
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
 
+  def start_game
+    @board.place_random_ships
+    p @board.num_ships
+    p @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      p 'you lose'
+      return true
+    else
+      return false
+    end
+  end
+
+  def win?
+    if @board.num_ships == 0
+      p 'you win'
+      return true
+    else
+      return false
+    end
+  end
+
+  def game_over?
+    win? || lose?
+  end
+
+  def turn
+    move = @player.get_move
+    if !@board.attack(move)
+      @remaining_misses -= 1
+    end
+    
+    p @remaining_misses
+    @board.print
+  end
 end
