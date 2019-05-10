24eb5e945a75209b1cf8bc06865be0f3ed73d65e
Kenneth Choi <kennethichoi@gmail.com>
2019-05-09 22:17:32 -0700

    Implement MaxHeap + #isMaxHeap

--- 08_interview_prep/04_graphs_heaps/01_heaps_project/lib/is_heap.js
+++ 08_interview_prep/04_graphs_heaps/01_heaps_project/lib/is_heap.js
@@ -1,9 +1,27 @@
 // you may assume that the array will always have a null element at the 0-th index
 function isMaxHeap(array, idx=1) {
+    if (array[idx] === undefined) return true;
 
+    let leftChildIdx = idx * 2;
+    let rightChildIdx = idx * 2 + 1;
+    let leftVal = array[leftChildIdx];
+    let rightVal = array[rightChildIdx];
+
+    if (leftVal === undefined) leftVal = -Infinity;
+    if (rightVal === undefined) rightVal = -Infinity;
+
+    return array[idx] > leftVal && array[idx] > rightVal &&
+        isMaxHeap(array, leftChildIdx) && 
+        isMaxHeap(array, rightChildIdx);
 }
 
 
 module.exports = {
     isMaxHeap
 };
+
+
+
+
+// [null, 100, 50, 25, 30, 40, 20, 10]; // => true
+
