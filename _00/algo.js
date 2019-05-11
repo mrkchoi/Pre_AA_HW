de04ec6f86d32b5ceb74dfc93622528693dd3986
Kenneth Choi <kennethichoi@gmail.com>
2019-05-10 21:14:26 -0700

    LC 53: Maximum Subarray Sequence v2 (js)

--- _00/algo.js
+++ _00/algo.js
@@ -1278,9 +1278,168 @@ var mergeTwoLists = function (l1, l2) {
 
 
 function minimumAbsoluteDifference(arr) {
+  let min = Infinity;
+  let set = new Set(arr);
+  
+  for (let i = 0; i < set.size - 1; i++) {
+    for (let j = i + 1; j < set.size; j++) {
+      let currentAbsDiff = Math.abs(arr[i] - arr[j]);
+      if (currentAbsDiff === 0) return 0;
+      if (currentAbsDiff < min) min = currentAbsDiff;
+    }
+  }
 
-
+  return min;
 }
+
+
+
+// Given an integer array nums, find the contiguous subarray(containing at least one number) which has the largest sum and return its sum.
+
+//   Example:
+
+// Input: [-2, 1, -3, 4, -1, 2, 1, -5, 4],
+// Output: 6
+// Explanation: [4, -1, 2, 1] has the largest sum = 6.
+
+// var maxSubArray = function (nums) {
+//   let maxToHere = nums[0];
+//   let maxSoFar = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     maxToHere = Math.max(maxToHere + nums[i], nums[i]);
+//     maxSoFar = Math.max(maxToHere, maxSoFar);
+//   }
+
+//   return maxSoFar;
+// };
+
+
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var maxSubArray = function (nums) {
+  let maxToHere = nums[0];
+  let maxSoFar = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    maxToHere = Math.max(maxToHere + nums[i], nums[i]);
+    maxSoFar = Math.max(maxToHere, maxSoFar);
+  }
+
+  return maxSoFar;
+};
+
+
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
